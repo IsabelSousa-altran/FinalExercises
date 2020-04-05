b6f059fc2fc2826b4eed25e495078d893ba42521
IsabelSousa-Altran <isabel.santossousa@altran.com>
2020-04-05 18:21:49 +0100

    Ex1 - Roman numbers

---
+++ NewExercises/NewExercises.Domain.Tests/RomanNumberTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using Bogus;
+using System;
+
+namespace NewExercises.Domain.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class RomanNumberTests
+    {
+
+        //[Fact]
+
+        //public void ConvertIntegerToRoman_Numberrange_ConvertSuccessfully()
+        //{
+        //    // Arrange
+        //    var number = new Faker().Random.Int(1, 3999);
+        //    var romanNumber = new RomanNumber();
+
+        //    // Act
+        //    romanNumber.ConvertIntegerToRoman(number);
+
+        //    // Assert
+        //    Assert.Equal(number, romanNumber);
+        //}
+
+        [Fact]
+        public void ConvertIntegerToRoman_NumberRange_MustBeAboveZero()
+        {
+            // Arrange
+            var number = new Faker().Random.Int(-1000, 0);
+            var romanNumber = new RomanNumber();
+
+            // Act
+            var exception = Assert.Throws<Exception>(() => romanNumber.ConvertIntegerToRoman(number));
+
+            // Assert
+            Assert.Equal("The number inserted is out of the limits", exception.Message);
+        }
+
+        public void ConvertIntegerToRoman_NumberRange_MustBeAbove4000()
+        {
+            // Arrange
+            var number = new Faker().Random.Int(4000, 9000);
+            var romanNumber = new RomanNumber();
+
+            // Act
+            var exception = Assert.Throws<Exception>(() => romanNumber.ConvertIntegerToRoman(number));
+
+            // Assert
+            Assert.Equal("The number inserted is out of the limits", exception.Message);
+        }
+
+
+    }
+}
+    
+
+
+
---
+++ NewExercises/NewExercises.Domain/RomanNumber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewExercises.Domain
+{
+    public class RomanNumber
+    {
+        //public int Number { get; set; }
+
+        //public RomanNumber(int number)
+        //{
+        //    Number = number;
+        //}
+
+        public  readonly Dictionary<int, string> NumberToRomanDictionary;
+
+        public  RomanNumber()
+        {
+            NumberToRomanDictionary = new Dictionary<int, string>
+            {
+                {1000, "M" },
+                {900, "CM" },
+                {500, "D" },
+                {400, "CD" },
+                {100, "C" },
+                {90, "XC" },
+                {50, "L" },
+                {40, "XL" },
+                {10, "X" },
+                {9, "IX" },
+                {5,"V" },
+                {4, "IV" },
+                {1, "I" },
+            };
+        }
+
+        public string ConvertIntegerToRoman(int integerNumber)
+        {
+            if ((integerNumber < 1) || (integerNumber > 3999))
+            {
+                throw new Exception("Insert values between 1 and 3999 ");
+            }
+
+
+            StringBuilder roman = new StringBuilder();
+
+            foreach (var item in NumberToRomanDictionary)
+            {
+                while (integerNumber >= item.Key)
+                {
+                    roman.Append(item.Value);
+                    integerNumber -= item.Key;
+                }
+            }
+            return roman.ToString();
+        }
+    }
+}
+
